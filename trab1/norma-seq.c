11da1e24fc4610e7ff1598bc0ccf9402509e67cf
Diogo Rocco <diogorocco12@gmail.com>
2021-04-22 10:24:31 -0300

    sequential implementation

---
+++ trab1/norma-seq.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<math.h>
+#include "utils.h"
+#include "timer.h"
+
+int matrix_dimension;
+double *input_matrix;
+double *normalized_matrix;
+
+void normalize_arrays(double* input_matrix, double* work_matrix){
+    for (int j = 0; j < matrix_dimension; j++){
+        double norm = 0;
+        for (int i = 0; i < matrix_dimension; i++){
+            double element = input_matrix[i*matrix_dimension + j];
+            norm += element*element; 
+        }
+
+        norm = sqrt(norm);
+        
+        for (int i = 0; i < matrix_dimension; i++)
+            work_matrix[i*matrix_dimension + j] = input_matrix[i*matrix_dimension + j]/norm;
+    }
+    
+}
+
+
+int main(int argc, char* argv[]){
+    double start_time, end_time;
+
+    if(argc<2) {printf("Digite %s <tamanho da matriz>\n", argv[0]); return 1;}
+    matrix_dimension = atoi(argv[1]);
+    
+    GET_TIME(start_time);
+    input_matrix = malloc(sizeof(double)*matrix_dimension*matrix_dimension);
+    if (input_matrix == NULL){printf("ERROR--malloc input\n"); return 2;}
+    normalized_matrix = malloc(sizeof(double)*matrix_dimension*matrix_dimension);
+    if (normalized_matrix == NULL){printf("ERROR--malloc normalized\n"); return 2;}
+    initialize_matrix(input_matrix, matrix_dimension);
+    GET_TIME(end_time);
+    printf("allocating time = %lf\n", end_time-start_time);
+    
+    
+    //printf("Input Matrix:\n");
+    
+    //print_matrix(input_matrix, matrix_dimension);
+    //puts("\nNormalized Arrays Matrix:");
+
+
+    GET_TIME(start_time);
+    normalize_arrays(input_matrix, normalized_matrix);
+    //print_matrix(normalized_matrix, matrix_dimension);
+    GET_TIME(end_time);
+
+    if(!check_correctness(normalized_matrix, matrix_dimension)){
+        printf("Wrong!!\n");
+        return 1;
+    }
+    printf("processing time = %lf\n", end_time-start_time);
+    log_execution_time("norma-seq-log", end_time-start_time);
+
+    free(normalized_matrix);
+    free(input_matrix);
+    return 0;
+}
