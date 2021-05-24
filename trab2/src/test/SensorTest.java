8041ddaf357d297071e304255e35194d190da679
diogo-rocco <diogorocco12@gmail.com>
2021-05-24 20:37:22 -0300

    making test best assert if method works

--- trab2/src/test/SensorTest.java
+++ trab2/src/test/SensorTest.java
@@ -35,11 +35,17 @@ public class SensorTest {
         Measurement[] buffer = new Measurement[5];
         Sensor sensor = new Sensor(0, buffer, new ThreadController());
 
-        for (int i=0; i<5; i++)
-            sensor.storeMeasurement(sensor.getMeasurement());
+        for (int i=0; i<5; i++) {
+            Measurement measurement = new Measurement(i + 1, 0, i * 5);
+            sensor.storeMeasurement(measurement);
+        }
 
-        for (int i=0; i<5; i++)
-            Assertions.assertTrue(buffer[i].getValue() >= 25 && buffer[i].getValue() <= 40);
+        for (int i=0; i<5; i++) {
+            Measurement measurement = new Measurement(i + 1, 0, i * 5);
+            Assertions.assertEquals(measurement.getValue(), buffer[i].getValue());
+            Assertions.assertEquals(measurement.getSensor_id(), buffer[i].getSensor_id());
+            Assertions.assertEquals(measurement.getMeasurement_id(), buffer[i].getMeasurement_id());
+        }
     }
 
 }
