ab272c210107660fcbfb3c7192b358894e514672
diogo-rocco <diogorocco12@gmail.com>
2021-05-23 21:56:57 -0300

    documenting Measurement class

--- trab2/src/main/resources/Measurement.java
+++ trab2/src/main/resources/Measurement.java
@@ -1,8 +1,19 @@
+package resources;
+
+/**
+ * Clase feita para funcionar como uma estrutura de dados para armazenar todas as informações de uma medição
+ */
 public class Measurement {
     private final int measurement_id;
     private final int sensor_id;
     private final double measurement_value;
 
+    /**
+     * construtor de um measurement
+     * @param measurement_id -> id da medição (gerado sequencialmente pelos sensores)
+     * @param sensor_id -> id do sensor que fez a medição
+     * @param value -> valor da medição
+     */
     public Measurement(int measurement_id, int sensor_id, double value){
         this.measurement_id = measurement_id;
         this.sensor_id = sensor_id;
